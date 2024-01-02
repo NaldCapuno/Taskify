f5ab1bc8f2eacda942d5f2cec9c765b2f52ab33e
NaldCapuno <jasoncapuno@gmail.com>
2024-01-02 19:50:31 +0800

    remove buttons border

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/main/static/'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
